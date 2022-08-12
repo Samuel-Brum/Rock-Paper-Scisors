30ffefc551e03ed26f76b80bae698bc325cba7f4
Samuel-Brum <samuelbrummartins@gmail.com>
2022-08-12 19:35:10 -0300

    completes function to play one round of the game

--- code.js
+++ code.js
@@ -4,7 +4,6 @@ function getComputerChoice() {
         1: "paper",
         2: "scissors"
     };
-
     let computerChoice = options[Math.floor((Math.random() * 10) % 3)]; 
     // selects random number between 0 and 2 inclusive
     // and uses it to select a value in options
@@ -12,29 +11,67 @@ function getComputerChoice() {
     return computerChoice;
 } // randomly returns rock, paper, or scisors
 
-function playerSelection() {
+function getPlayerChoice() {
     do {
         playerChoice = prompt("Rock, paper, or scissors?");
         playerChoice = playerChoice.toLowerCase();
     } while (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors");
-
-    console.log(playerChoice);
+    console.log("player choice is " + playerChoice);
     return playerChoice;
 } // queries the player for a adequate input
   // the input is sanitized by this function
 
 function playRound(playerSelection, computerSelection) {
+    let  options = {
+        0: "rock",
+        1: "paper",
+        2: "scissors"
+    };
+    let match = [
+        computerSelection,
+        playerSelection
+    ]
 
+    if (match[0] === match[1]) {
+        return "It's a tie!";
+        console.log("It's a tie!");
+    }
+    switch (match[0]) {
+        case "rock":
+            switch (match[1]) {
+                case "paper":
+                    return "You win! Paper beats rock!!";
+                case "scissors":
+                    return "You lose... Rock beats scissors";
+            };
+        case "paper":
+            switch (match[1]) {
+                case "rock":
+                    return "You lose... paper beats rock!";
+                case "scissors":
+                    return "You win! Scissors beats paper!";
+            };
+        case "scissors":
+            switch (match[1]) {
+                case "paper":   
+                    return "You lose... scissors beats paper!";
+                case "rock":
+                    return "You win! Rock beats Scissors!"
+            };
+        };
 } // plays one round of an input by the player 
   // agaist a random choice by the computer
 
 function test() {
-    const playerSelection = "rock";
+    const playerSelection = getPlayerChoice();
     const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
+    result = playRound(playerSelection, computerSelection)
+    console.log(result);
+    return result;
 } // tests a round of the game against 'rock' input
 
 function game() {
+
 
 } // plays a 5 round match
 
